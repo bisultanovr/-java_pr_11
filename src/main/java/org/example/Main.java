dc858551886e7ccec3cb6637b13a0ac68bcc0018
Radzhab Bisultanov <bisultanov887@gmail.com>
2025-10-26 21:56:26 +0300

    Реализована основая логика работы программы

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Main {
     public static class Stats {
         public long lines;
@@ -30,7 +37,54 @@ public class Main {
 
         return s;
     }
-    public static void main(String[] args) {
 
+    private static void printUsage() {
+        System.out.println("Для запуска в качестве аргумента необходимо указать путь к исходному файлу.");
+        System.out.println("Чтобы вывести результат работы программы в файл необходимо после исходного файла указать флаг --out, затем путь, по которому нужно сохранить файл.");
+        System.out.println("Если не указать флаг --out, результат программы выведется в консоль.");
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            printUsage();
+            System.exit(1);
+        }
+
+        Path input = null;
+        Path out = null;
+
+        try {
+            input = Paths.get(args[0]);
+
+            if (!Files.exists(input) || !Files.isRegularFile(input)) {
+                System.err.println("Такого файла нет или это не стандартный файл");
+                System.exit(2);
+            }
+
+            if (args.length == 3 && "--out".equals(args[1])) {
+                out = Paths.get(args[2]);
+            }
+            else {
+                System.err.println("Неизвестный флаг" + args[1]);
+                printUsage();
+                System.exit(3);
+            }
+
+            byte[] bytes = Files.readAllBytes(input);
+            String content = new String(bytes, StandardCharsets.UTF_8);
+
+            Stats stats = analyze(content);
+
+            System.out.println("Статистика файла: " + input.toString());
+            System.out.println(stats.toString());
+
+            if (out != null) {
+                String outText = "Статистика файла: " + input.toString() + System.lineSeparator() + stats.toString();
+                Files.writeString(out, outText, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                System.out.println("Статистика сохранена в файл: " + out.toString());
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка ввода/вывода: " + e.getMessage());
+        }
     }
 }
