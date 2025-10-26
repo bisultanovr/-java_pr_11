431122c22b29ee3e42865ddbf78f729c917ea9e9
Radzhab Bisultanov <bisultanov887@gmail.com>
2025-10-26 21:26:24 +0300

    Добавлен класс Stats, содержащий вычисляемые данные. Метод analyze, который производит вычисления

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
 public class Main {
+    public static class Stats {
+        public long lines;
+        public long words;
+        public long characters;
+
+        @Override
+        public String toString() {
+            return String.format("Lines: %d%nWords: %d%nCharacters: %d%n", lines, words, characters);
+        }
+    }
+
+    public static Stats analyze(String content) {
+        Stats s = new Stats();
+
+        String[] lines = content.split("\\R", -1);
+        s.lines = lines.length;
+
+        String trimmed = content.trim();
+        if (trimmed.isEmpty()) {
+            s.words = 0;
+        } else {
+            String[] words = trimmed.split("\\s+");
+            s.words = words.length;
+        }
+
+        s.characters = content.length();
+
+        return s;
+    }
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
     }
 }
